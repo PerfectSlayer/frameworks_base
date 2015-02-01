ebb6340ce1cb33ad81967962067764567ea4c049
PerfectSlayer <slimroms@hardcoding.fr>
2015-02-01 15:29:24 +0100

    Add default traffic display
    Change-Id: Ice8c38c2c124582420dd362d4f8609a86ad9070e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2154,6 +2154,12 @@ public final class Settings {
         public static final String STATUS_BAR_TICKER_ENABLED = "status_bar_ticker_enabled";
 
         /**
+         * Network speed indicator.
+         * @hide
+         */
+        public static final String STATUS_BAR_TRAFFIC = "status_bar_traffic";
+
+        /**
          * Control whether the process CPU usage meter should be shown.
          *
          * @deprecated Use {@link Global#SHOW_PROCESSES} instead
